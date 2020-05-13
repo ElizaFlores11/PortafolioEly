19eda8aa1d16a62c4a2e50a93631506b54c790b7
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-05-13 16:35:51 -0500

    Rutas

--- docs/index.js
+++ docs/index.js
@@ -25,32 +25,3 @@ const router = (route) => {
 }
 btns.forEach(btn => btn.onclick = e => router(e.target.id));
 router(); 
-//console.log(btns); 
-
-
-/*let start = document.querySelector('#start'); 
-let about = document.querySelector('#about');
-let skills = document.querySelector('#skills');
-let proyects = document.querySelector('#proyects');
-let contact = document.querySelector('#contact');
-let contentStart = document.querySelector("contentStart"); 
-let contentAbout = document.querySelector("contentAbout");
-let contentSkills = document.querySelector("contentSkills");
-let contentProyects = document.querySelector("contentProyects");
-let contentContact= document.querySelector("contentContact");
-
-start.addEventListener('click', () =>{
-  alert('Start');
-}); 
-about.addEventListener('click', () =>{
-  console.log('About');
-}); 
-skills.addEventListener('click', () =>{
- contentProyects.style.display = 'block'; 
-});
-proyects.addEventListener('click', () =>{
-  contentProyects.style.display = 'block'; 
-}); 
-contact.addEventListener('click', () =>{
-  console.log('Contact'); 
-});*/
