3392ab38960cef8633b9c9b9f404f24c1cf2b8b8
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-05-13 12:15:33 -0500

    project complement

--- README.md
+++ README.md
@@ -3,17 +3,24 @@ Portafolio de proyectos desarrollados.
 
 ## Inicio
 
+Muestra la pantalla de inicion, del portafolio 
+
 <img src="prototipe/inicio.png">
 
-
 ## Soft Skills
+
+Al dar click del menú en "Soft skill", se muestrara una pantalla donde en listar las habilidades blandas.
 
 <img src="prototipe/soft.png">
 
 ## Tech Skill
 
+Si se da clikk en "Teck skill", se en lista los nombre de las herramientas con las que se ha trabajado en diferentes proyectos.
+
 <img src="prototipe/tech.png">
 
 # Proyectos
+
+Al dar clik en "Proyect", muestra el nombre y descripcion de los proyecto que se han generando.
 
 <img src="prototipe/proyect.png">
--- src/proyect.js
+++ src/proyect.js
@@ -5,7 +5,7 @@ export const renderProyect = () =>{
             <h2>TRIPLIFE </h2>
             <p>Social Network, para viajeros creada para compartir y recordar tus historias,
              fotos y tips con otros viajeros que disfrutan vivir a través de los viajes. </br> </p>
-             <a href="#" class="links">Repositorio</a>
+             <a href="https://github.com/ElizaFlores11/cipher/tree/master/src" target="_black" class="links">Repositorio</a>
              <a href="#" class="links">Demo</a>
              </br></br></br>
         </div>
@@ -13,7 +13,7 @@ export const renderProyect = () =>{
             <h2> RICK & MORTY</h2>
             <p> Data de Rick and Morty, para seguir aprendiendo DOM, HTML y Js Se trabajo en dupla para poder avanzar y 
             poder entender como piensa, buena comunicación con las compañeras</p>
-            <a href="#"  class="links">Repositorio</a>
+            <a href="https://github.com/ElizaFlores11/CDMX009-Data-Lovers" target="_black"  class="links">Repositorio</a>
              <a href="#"  class="links">Demo</a>
              </br></br></br>
         </div>
@@ -21,7 +21,7 @@ export const renderProyect = () =>{
             <h2>BULLYING SCHOOL </h2>
             <p>Se utiliza para enviar mensaje ocultos a los profesores, cuando los estudiantes 
             son amenazados y tienen miedo de expresarlo a un profesor o tutor</p>
-            <a href="#"  class="links">Repositorio</a>
+            <a href="https://github.com/ElizaFlores11/CDMX009-Social-Network"  class="links">Repositorio</a>
              <a href="#"  class="links">Demo</a>
              </br></br></br>
         </div>
